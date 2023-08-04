9f9bb3069f6d92e646a2987aed60b00685368212
OmeshSachethana <sachethana12@gmail.com>
2023-08-04 17:19:39 +0530

    updated

--- src/main/java/com/test/model/Employee.java
+++ src/main/java/com/test/model/Employee.java
@@ -1,7 +1,26 @@
 package com.test.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
 
+    public Employee() {}
 }
